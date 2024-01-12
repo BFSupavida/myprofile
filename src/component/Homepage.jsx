f90f110db2b8413b4f8f533dd5e83169c0a21d58
BFSupavida <F.Supavida@gmail.com>
2024-01-13 00:44:33 +0700

    contetnt(homepage): add content intro

---
+++ src/component/Homepage.jsx
@@ -0,0 +1,17 @@
+import Layout from "./Layout";
+
+
+const Homepage = () => {
+    return (
+      <Layout>
+        <img alt="myself" />
+        <h1>Supavida Itthirak (BaiFern). </h1>
+        <p>
+        I've been learning to code for 4 month after switching careers. 
+        I started with HTML, CSS and JavaScript by joining Junior Software Developer Generation Thailand Bootcamp Cohort 6.
+        Currently, I am looking for opertu</p>
+      </Layout>
+    );
+  };
+  
+  export default Homepage;
